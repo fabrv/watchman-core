ae9bd28379d50fe8e2a1f2fe7a5817d4ce071a39
fabrv <fabrizzio.rivera@telus.com>
2022-05-13 10:42:09 -0600

    feat: top bar

---
+++ src/stories/TopBar/TopBar.stories.tsx
@@ -0,0 +1,50 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { FiLogOut, FiSettings } from 'react-icons/fi'
+
+import { TopBar } from './TopBar'
+
+export default {
+  title: 'Core/TopBar',
+  component: TopBar,
+  parameters: {
+    layout: 'fullscreen'
+  }
+} as ComponentMeta<typeof TopBar>
+
+const Template: ComponentStory<typeof TopBar> = (args) => <TopBar {...args}/>
+
+export const Light = Template.bind({})
+Light.args = {
+  brand: '⏱ Watchman',
+  links: [
+    {
+      title: '',
+      Icon: FiLogOut,
+      href: '#'
+    },
+    {
+      title: '',
+      Icon: FiSettings,
+      href: '#'
+    }
+  ],
+  variant: 'light'
+}
+
+export const Dark = Template.bind({})
+Dark.args = {
+  brand: '⏱ Watchman',
+  links: [
+    {
+      title: '',
+      Icon: FiLogOut,
+      href: '#'
+    },
+    {
+      title: '',
+      Icon: FiSettings,
+      href: '#'
+    }
+  ],
+  variant: 'dark'
+}
---
+++ src/stories/TopBar/TopBar.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from 'react'
+import { Col, Container, Nav, Navbar } from 'react-bootstrap'
+import { IconType } from 'react-icons/lib'
+
+export interface TopBarProps {
+  brand: string
+  showNav?: boolean,
+  children: ReactNode,
+  variant?: 'dark' | 'light'
+  links?: {
+    title: string,
+    Icon: IconType,
+    href: string
+  }[]
+}
+
+export const TopBar = ({ brand, showNav = true, links, variant = 'light' }: TopBarProps) => {
+  return (
+    <Navbar variant={variant}>
+      <Container fluid={true} className='no-gutter'>
+        <Col md={2}>
+          <Navbar.Brand>{brand}</Navbar.Brand>
+        </Col>
+        {
+          showNav &&
+          (
+            <Nav className="justify-content-end">
+              {
+                links && links.map(({ title, Icon, href }) => (
+                  <Nav.Link key={href} href={href}>
+                    <Icon />
+                    {title}
+                  </Nav.Link>
+                ))
+              }
+            </Nav>
+          )
+        }
+      </Container>
+    </Navbar>
+  )
+}
